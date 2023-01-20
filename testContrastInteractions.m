d0cca7932fbcfbeab88dc9c2d6d1f3b640a07f51
Angus Chapman <angusdotchapman@gmail.com>
2023-01-20 11:58:35 -0500

    plot neutral condition with T1/T2 contrast varied

--- testContrastInteractions.m
+++ testContrastInteractions.m
@@ -1,9 +1,10 @@
 % model testing
 opt = [];
 % H/H, H/L, L/H, L/L
-opt.stimContrasts = [.64 .64 .32 .32; ...
-                     .64 .32 .64 .32];
+opt.stimContrasts = [.64 .64 .16 .16; ...
+                     .64 .16 .64 .16];
 
+modelClass = [];
 rsoa = 4; % SOA = 250 ms
 rseq = []; % default orientation sequence
 rcond = []; % cueT1, cueT2, cueN
@@ -23,7 +24,7 @@ sortedPerf(2,1,:) = perf(2,2,:); % T2 valid
 sortedPerf(2,2,:) = perf(2,3,:); % T2 neutral
 sortedPerf(2,3,:) = perf(2,1,:); % T2 invalid
 
-conds = ["high/high","high/low","low/high","low/low"];
+conds = ["T1 high/T2 high","T1 high/T2 low","T1 low/T2 high","T1 low/T2 low"];
 figure
 for ii=1:4
     subplot(1,4,ii)
@@ -33,3 +34,19 @@ for ii=1:4
     ylabel("Performance (d')"), ylim([0 3])
 end
 legend(["T1","T2"])
+
+% neutral condition only
+figure
+subplot(1,2,1), hold on, title("T1")
+plot(1:2, squeeze(sortedPerf(1,2,[3 1]))) % T1 with T2 high contrast
+plot(1:2, squeeze(sortedPerf(1,2,[4 2]))) % T1 with T2 low contrast
+xlim([0.8 2.2]), xticks(1:2), xticklabels(["T1 low","T1 high"])
+ylim([0 3]), ylabel("Performance (d')")
+legend(["T2 high","T2 low"])
+
+subplot(1,2,2), hold on, title("T2")
+plot(1:2, squeeze(sortedPerf(2,2,[2 1]))) % T2 with T1 high contrast
+plot(1:2, squeeze(sortedPerf(2,2,[4 3]))) % T2 with T1 low contrast
+xlim([0.8 2.2]), xticks(1:2), xticklabels(["T2 low","T2 high"])
+ylim([0 3]), ylabel("Performance (d')")
+legend(["T1 high","T1 low"])
