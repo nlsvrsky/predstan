2eb63a47b5633bca738d62b985f2daa84407fb16
Angus Chapman <angusdotchapman@gmail.com>
2024-08-28 14:06:33 -0400

    update model responses with varying time constants

--- testStimResp.m
+++ testStimResp.m
@@ -2,7 +2,7 @@
 
 opt = [];
 modelClass = [];
-rsoa = 10; % SOA = 250 ms (see runModel)
+rsoa = 1001; % SOA = 250 ms (see runModel)
 rseq = []; % default orientation sequence
 rcond = 3; % cueT1, cueT2
 soas = [100:50:500 800];
@@ -14,7 +14,7 @@ soas = [100:50:500 800];
 % opt.stimContrasts = [.64 .64 .32 .32; ...
 %                      .64 .32 .64 .32];
 opt.stimContrasts = [.64; 0];
-opt.stimDur = 500;
+opt.stimDur = 1000;
 opt.scaling1 = 5e5;
 opt.scaling2 = 6e5;
 % opt.aAI = 1e2;
@@ -47,10 +47,20 @@ opt.aAV = 2e2;
 % opt.AVNeutralT1Weight = 0.25;
 % opt.distributeVoluntary = 0;
 
+opt.dt = 2;
+opt.T = 4.1*1000;
+opt.nt = opt.T/opt.dt+1;
+opt.tlist = 0:opt.dt:opt.T;
+
 opt.display.plotTS = 0; % plot the time series for each simulation
 opt.display.plotPerf = 0;
 
 tauE_list = [50 100:100:800];
+r1_E = nan(12,opt.nt,length(tauE_list));
+r1_S = nan(12,opt.nt,length(tauE_list));
+d1_S = nan(12,opt.nt,length(tauE_list));
+s1_S = nan(12,opt.nt,length(tauE_list));
+f1_S = nan(12,opt.nt,length(tauE_list));
 
 for ii=1:length(tauE_list)
     opt.tauE1 = tauE_list(ii);
@@ -65,19 +75,29 @@ for ii=1:length(tauE_list)
     d1_S(:,:,ii) = p.d1;
     s1_S(:,:,ii) = p.s1;
     f1_S(:,:,ii) = p.f1;
-
-    rai(:,:,ii) = p.rai;
-    dai(:,:,ii) = p.dai;
-    sai(:,:,ii) = p.sai;
-    fai(:,:,ii) = p.fai;
 end
 
 figure
 subplot(121)
-plot(p.tlist,squeeze(r1_E(6,:,:)))
+plot(p.tlist,squeeze(r1_E(6,:,:)./max(r1_E(6,:,:),[],2)))
 
 subplot(122)
-plot(p.tlist,squeeze(r1_S(6,:,:)))
+plot(p.tlist,squeeze(r1_S(6,:,:)./max(r1_S(6,:,:),[],2)))
+
+% examples of parameter combinations
+tauE_list = [100 100 500 500];
+tauS_list = [50  500  50 500];
+
+for ii=1:length(tauE_list)
+    opt.tauE1 = tauE_list(ii);
+    opt.tauS1 = tauS_list(ii);
+    [~,p,perf] = runModel(opt, modelClass, rsoa, rseq, rcond);
+    r1_comb(:,:,ii) = p.r1;
+end
+
+figure
+plot(p.tlist,squeeze(r1_comb(6,:,:)./max(r1_comb(6,:,:),[],2)))
+
 
 %% subadditive responses
 
