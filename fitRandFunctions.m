e2cd4c6486981f12c273e6681aa43a01b92f7e42
Angus Chapman <angusdotchapman@gmail.com>
2025-02-05 14:36:38 -0500

    add code to generate main analyses

--- fitRandFunctions.m
+++ fitRandFunctions.m
@@ -1,31 +1,42 @@
 function fitRandFunctions
+% this function fits simplified difference of Gamma functions to the output
+% from the reverse correlation analysis across different parameter
+% combinations (see: testRandomSeq.m) and was run on the BU SCC
+
+% data output is available on our OSF repository:
 
 ncores = str2num(getenv("NSLOTS"));
 pool = parpool(ncores);
 
+%% do reverse correlation and fit diff of Gammas
 t = -3000:5:0;
 
 fitParams = nan(4,100);
 
-r1s = nan(100,length(t));
-d1s = nan(100,length(t));
-s1s = nan(100,length(t));
-f1s = nan(100,length(t));
+r1s = nan(100,length(t)); % sensory layer response
+d1s = nan(100,length(t)); % excitatory drive
+s1s = nan(100,length(t)); % suppressive drive
+f1s = nan(100,length(t)); % normalization factor
 
 parfor ii=1:100
-    tempOut = load(sprintf('output/randSequence/rand_out_%03d.mat',ii),'out');
+    % for each parameter combination we need to load in the data from the
+    % array job
+    tempOut = load(sprintf('output/randomSeq/rand_out_%03d.mat',ii),'out');
 
-    r1_rc = sum(tempOut.r1(:,end).*tempOut.stimList)./sum(tempOut.stimList)-mean(tempOut.r1(:,end));
+    % see Eqn 8 in the manuscript
+    r1_rc = 1e3 * (sum(tempOut.out.r1(:,end).*tempOut.out.stimList)./sum(tempOut.out.stimList)-mean(tempOut.out.r1(:,end)));
     
-    diffOfGamma = @(x) x(4) .* (t.*exp(t./x(1))-x(3).*t.*exp(t./x(2)));
-    funcSSE = @(x) sum((diffOfGamma(x)-r1_rc).^2);
+    % simplified difference of Gammas from Zhou et al (2019) in PLoS Comp Bio
+    diffOfGamma = @(t1,t2,w) t.*exp(t./t1) - w.*t.*exp(t./t2);
+    funcSSE = @(x) sum((x(4).*diffOfGamma(x(1),x(2),x(3))-r1_rc).^2);
 
     funcFit = nan(4,100);
     funcVal = nan(1,100);
     
+    % fit functions with 100 random starts and pick the best fit
     for rr=1:100
         [tempFit,tempVal] = fminsearch(funcSSE,[randi(900),randi(900),1,1], ...
-            optimset('MaxIter',1e4,'MaxFunEvals',1e4,'TolX',1e-12,'TolFun',1e-12));
+            optimset('MaxIter',1e5,'MaxFunEvals',1e5,'TolX',1e-12,'TolFun',1e-12));
         funcFit(:,rr) = tempFit;
         funcVal(rr) = tempVal;
     end
@@ -33,10 +44,11 @@ parfor ii=1:100
     bestFit = funcFit(:,find(funcVal==min(funcVal),1));
     fitParams(:,ii) = bestFit;
 
+    % reverse correlation analysis output for each variable
     r1s(ii,:) = r1_rc;
-    d1s(ii,:) = sum(tempOut.d1(:,end).*tempOut.stimList)./sum(tempOut.stimList)-mean(tempOut.d1(:,end));
-    s1s(ii,:) = sum(tempOut.s1(:,end).*tempOut.stimList)./sum(tempOut.stimList)-mean(tempOut.s1(:,end));
-    f1s(ii,:) = sum(tempOut.f1(:,end).*tempOut.stimList)./sum(tempOut.stimList)-mean(tempOut.f1(:,end));
+    d1s(ii,:) = sum(tempOut.out.d1(:,end).*tempOut.out.stimList)./sum(tempOut.out.stimList)-mean(tempOut.out.d1(:,end));
+    s1s(ii,:) = sum(tempOut.out.s1(:,end).*tempOut.out.stimList)./sum(tempOut.out.stimList)-mean(tempOut.out.s1(:,end));
+    f1s(ii,:) = sum(tempOut.out.f1(:,end).*tempOut.out.stimList)./sum(tempOut.out.stimList)-mean(tempOut.out.f1(:,end));
 end
 
 out.fitParams = fitParams;
--- testRandomSeq.m
+++ testRandomSeq.m
@@ -1,31 +1,40 @@
+function testRandomSeq
+% this function generates model responses for reverse correlation analyses
+% and was run through the Boston University SCC - adjustments to the code
+% are likely necessary to reproduce the analyses
 
+% data output is available on our OSF repository:
+
+ncores = str2num(getenv("NSLOTS"));
+pool = parpool(ncores);
+fprintf("Parpool started\n");
+
+%% model setup
 opt = [];
 modelClass = [];
-rsoa = 10; % SOA = 250 ms (see runModel)
+rsoa = []; % SOA = 250 ms (see runModel)
 rseq = []; % default orientation sequence
 rcond = 3; % cueT1, cueT2
 
-% H/H, H/L, L/H, L/L
 opt.stimContrasts = [.64; .64];
 opt.scaling1 = 3e5;
 opt.scaling2 = 4e5;
 opt.aAI = 0;
 opt.aAV = 0;
 
-opt.eScale = 1;
-opt.sScale = 50;
-
-opt.tauE1 = 0;
-opt.tauS1 = 0;
+opt.sigma1 = 0.1;
 
 opt.dt = 5;
-opt.T = 1*1200;
+opt.T = 1*3000;
 opt.nt = opt.T/opt.dt+1;
 opt.tlist = 0:opt.dt:opt.T;
+
+opt.stimMode = 'random'; % generate random stim sequences
 
 opt.display.plotTS = 0; % plot the time series for each simulation
 opt.display.plotPerf = 0;
 
+%% generate model output to random sequences
 nSim = 1e4;
 
 stimList = nan(nSim,opt.nt);
@@ -34,16 +43,31 @@ s1_out = nan(nSim,opt.nt);
 f1_out = nan(nSim,opt.nt);
 r1_out = nan(nSim,opt.nt);
 
-paramList = combvec(1:nSim,[400; 100]);%[0 100 400; 0 400 100]);
+% code was originally run as an array job, with each parameter combination
+% executed and output saved separately
+paramList = combvec(0:100:900,0:100:900);
+paramVal = str2num(getenv("SGE_TASK_ID"));
 
-parfor ii=1:length(paramList)
-    opt2 = opt;
-    opt2.tauE1 = paramList(2,ii);
-    opt2.tauS1 = paramList(3,ii);
-    [~,p,~] = runModel(opt2, modelClass, rsoa, rseq, rcond);
+opt.tauE1 = paramList(1,paramVal);
+opt.tauS1 = paramList(2,paramVal);
+
+rng('shuffle');
+
+parfor ii=1:nSim
+    [~,p,~] = runModel(opt, modelClass, rsoa, rseq, rcond);
     stimList(ii,:) = p.stim(1,:);
     d1_out(ii,:) = p.d1(6,:);
     s1_out(ii,:) = p.s1(6,:);
     f1_out(ii,:) = p.f1(6,:);
     r1_out(ii,:) = p.r1(6,:);
 end
+
+fprintf('Sims done\n');
+
+out.r1 = r1_out;
+out.d1 = d1_out;
+out.s1 = s1_out;
+out.f1 = f1_out;
+out.stimList = stimList;
+
+save(sprintf('output/randomSeq/rand_out_%03d.mat',paramVal),'out');
