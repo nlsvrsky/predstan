0deb8b2571262cc09ba0d09f134b8a0e9b6044a2
Angus Chapman <angusdotchapman@gmail.com>
2025-02-05 11:25:22 -0500

    move core model functions to new folder

---
+++ model/setStim.m
@@ -0,0 +1,24 @@
+function p = setStim(p)
+
+if strcmp(p.stimMode,'standard')
+    % start and end times of T1
+    stimStart = p.stimOnset;
+    stimEnd = p.stimOnset + p.stimDur;
+
+    % make stim
+    timeSeries = zeros([p.norient p.nt]);
+    timeSeries(p.stimseq(1),unique(round((stimStart:p.dt:stimEnd)/p.dt))) = 1; % T1
+    timeSeries(p.stimseq(2),unique(round(((stimStart:p.dt:stimEnd) + p.soa)/p.dt))) = 1; % T2
+
+    % stimulus contrasts
+    contrSeries = zeros([p.norient p.nt]);
+    contrSeries(p.stimseq(1),unique(round((stimStart:p.dt:stimEnd)/p.dt))) = p.contrast(1); % T1
+    contrSeries(p.stimseq(2),unique(round(((stimStart:p.dt:stimEnd) + p.soa)/p.dt))) = p.contrast(2); % T2
+
+    p.stim = timeSeries;
+    p.stimContrast = contrSeries;
+else % random stim array
+    p.stim = zeros(4,length(p.tlist));
+    p.stim(1,:) = randi([0 1],1,length(p.tlist));
+    p.stimContrast = p.stim;
+end
